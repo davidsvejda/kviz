60891e41a0e0e957e58a4c8c3556bfaddedb5735
David Svejda <david@svejda.org>
2023-05-09 16:02:16 +0200

    Added quiz04

---
+++ quiz04.js
@@ -0,0 +1,125 @@
+const quizData = [
+    {
+        question: "Křestní jméno kontrolora?",
+        image: "images/pic61.jpg",
+        answer: "Evžen",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Profese kontrolorova zeťáka?",
+        image: "images/pic62.jpg",
+        answer: "taxikář",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Co stěhovali po schodech nahoru?",
+        image: "images/pic63.jpg",
+        answer: "gauč",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Název vesnice, kde má Huml chalupu?",
+        image: "images/pic64.jpg",
+        answer: "Třešňová",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Kdo hraje milence Fuchsové? Celé jméno.",
+        image: "images/pic65.jpg",
+        answer: "Jiří Adamíra",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Co odnesou z chalupy před oslavou na hasičském bále, kde má Bohouš dostat uznání?",
+        image: "images/pic66.jpg",
+        answer: "orchestrion",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Jaké jídlo pan Huml s Bohoušem připravují?",
+        image: "images/pic67.png",
+        answer: "bramboráky",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Jaké zvíře je zdrojem konfliktu v tomto díle?",
+        image: "images/pic68.jpg",
+        answer: "prase",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Celé jméno herečky zakryté na obrázku, která stojí vedle Jiřího Schmitzera?",
+        image: "images/pic69.jpg",
+        answer: "Hana Maciuchová",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Příjmení postavy, která se neočekávaně vynoří z koupelny u paní Míly, když je tam autobusák Čihák?",
+        image: "images/pic70.jpg",
+        answer: "Motl",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Kdo režíruje divadelní hru, Bohouš nebo Huml?",
+        image: "images/pic71.jpg",
+        answer: "Bohouš",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Název země, ve které byl Bohouš se svým kamarádem na montážích?",
+        image: "images/pic72.jpg",
+        answer: "Turecko",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Huml: Jestli ten tvůj kamarád ten ... jenom simuluje. Co simuluje?",
+        image: "images/pic73.png",
+        answer: "žlučník",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Huml potřebuje tisíc cihel na stavbu u rybníka a předseda mu řekne, ať si je vezme u...?",
+        image: "images/pic74.jpg",
+        answer: "kravína",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Kolik svateb v tomto díle probíhá? Napiš číslo.",
+        image: "images/pic75.jpg",
+        answer: "2",
+        details: "Seriál: Chalupáři (1975). Balabán (Prachař) a Fuchsová (Bohdalová), Krbec (Schmizter) a Machovcová (Maciuchová) "
+    },
+    {
+        question: "Zatím co stojím na...?",
+        image: "images/pic76.png",
+        answer: "taburetu",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Celé jméno herečky, která hraje předsedovu dceru Milušku.",
+        image: "images/pic77.jpg",
+        answer: "Mahulena Bočanová",
+        details: "Seriál: Chalupáři (1975)."
+    },
+    {
+        question: "Postava Františka Filipovského se jmenuje Alfons Karásek? Ano/Ne",
+        image: "images/pic78.jpg",
+        answer: "Ne",
+        details: "Seriál: Chalupáři (1975). Postava se jmenuje Lojza Klásek."
+    },
+    {
+        question: "Nám stačí, když pojedeme až v...? V kolik? Napiš slovy.",
+        image: "images/pic79.png",
+        answer: "pět",
+        details: "Seriál: Chalupáři (1975). Proč ráno? Proč brzy? Správně! Nám stačí, když pojedeme až v pět."
+    },
+    {
+        question: "Huml: Ještě si zazvoň, ...! Doplň, co Huml zakřičel.",
+        image: "images/pic80.png",
+        answer: "zatracenej parchante",
+        details: "Seriál: Chalupáři (1975)."
+    }
+    // Add more questions as needed
+];
+
+window.quizData = quizData;
