5e587637781b1263d9cde24bfd3d4b9c62d066d4
David Svejda <david@svejda.org>
2023-05-09 12:03:27 +0200

    Added quiz03

---
+++ quiz03.js
@@ -0,0 +1,125 @@
+const quizData = [
+    {
+        question: "Název expedice, na kterou se každoročně vypravují?",
+        image: "images/pic41.jpg",
+        answer: "YDYKSEB",
+        details: "Film: S tebou mě baví svět (1982). Každý rok jezdí do Beskyd, název expedice je Beskydy pozpátku."
+    },
+    {
+        question: "Celé jméno herce, který ztvárnil roli vedoucího kosmetického salonu?",
+        image: "images/pic42.jpg",
+        answer: "Juraj Herz",
+        details: "Film: Což takhle dát si špenát (1977)."
+    },
+    {
+        question: "Příjmení herce vpravo na obrázku?",
+        image: "images/pic43.png",
+        answer: "Brodský",
+        details: "Film: Zítra vstanu a opařím se čajem (1956)."
+    },
+    {
+        question: "Vy nás ale zásobujete, pane...?",
+        image: "images/pic44.png",
+        answer: "Karfík",
+        details: "Film: Jáchyme, hoď ho do stroje! (1974). Chocholoušek si pochvaluje spolupráci s vedoucím autoservisu Karfíkem."
+    },
+    {
+        question: "Jak se jmenuje komisař na obrázku?",
+        image: "images/pic45.jpg",
+        answer: "Ledvina",
+        details: "Film: Adéla ještě nevečeřela (1977)."
+    },
+    {
+        question: "Jak se jmenuje postava, kterou hrál Josef Hlinomaz?",
+        image: "images/pic46.jpg",
+        answer: "Gogo",
+        details: "Film: Čtyři vraždy stačí, drahoušku (1970). My jsme všichni Gogo."
+    },
+    {
+        question: "Jaké slovo řekne jednou za 100 km postava vlevo?",
+        image: "images/pic47.jpg",
+        answer: "Beroun",
+        details: "Film: Vesničko má středisková (1985)."
+    },
+    {
+        question: "Jak se jmenuje jejich pes?",
+        image: "images/pic48.jpg",
+        answer: "Pinďa",
+        details: "Film: Slunce, seno, jahody (1983)."
+    },
+    {
+        question: "Jak se jmenují sousedi Novákových?",
+        image: "images/pic49.jpg",
+        answer: "Bartáčkovi",
+        details: "Film: Zítra to roztočíme, drahoušku...! (1976)."
+    },
+    {
+        question: "Augustín Kubán zde hrál baronova sluhu Zutra, zvaného Tóma...?",
+        image: "images/pic50.jpg",
+        answer: "Hluchoněmec",
+        details: "Film: Tajemství hradu v Karpatech (1981)."
+    },
+    {
+        question: "Název města, do kterého byl nadporučík Lukáš za trest přeložen?",
+        image: "images/pic51.jpg",
+        answer: "České Budějovice",
+        details: "Film: PDobrý voják Švejk (1956)."
+    },
+    {
+        question: "Jak se jmenuje bratr správce pivovaru?",
+        image: "images/pic52.jpg",
+        answer: "Pepin",
+        details: "Film: Postřižiny (1980). Správce pivovaru Francin (Schmitzer), jeho bratr Pepin (Hanzlík)."
+    },
+    {
+        question: "Chatová oblast...?",
+        image: "images/pic53.jpg",
+        answer: "Kersko",
+        details: "Film: Slavnosti sněženek (1983)."
+    },
+    {
+        question: "Celé jméno herce, který hraje medika Boba Karáska?",
+        image: "images/pic54.jpg",
+        answer: "Oldřich Vízner",
+        details: "Film: Léto s kovbojem (1976)."
+    },
+    {
+        question: "Název filmu?",
+        image: "images/pic55.jpg",
+        answer: "Rozpuštěný a vypuštěný",
+        details: "Film: Rozpuštěný a vypuštěný (1984)."
+    },
+    {
+        question: "Jak se jmenuje prodavačka vpravo?",
+        image: "images/pic56.jpg",
+        answer: "Douchová",
+        details: "Film: Vrchní, prchni! (1980)."
+    },
+    {
+        question: "Hraje v tomto filmu Karel Augusta? Ano/Ne",
+        image: "images/pic57.jpg",
+        answer: "Ano",
+        details: "Film: Kamarád do deště (1988). Karel Augusta hraje vrchního Kadlece."
+    },
+    {
+        question: "Celkem kolik domácností zde směňuje byt? Napiš číslo.",
+        image: "images/pic58.jpg",
+        answer: "12",
+        details: "Film: Kulový blesk (1980). Bytová dvanáctisměna je největší akce v dějinách stěhování."
+    },
+    {
+        question: "Jak se jmenuje zedník, kterého se sousedka ptala, jestli to uveze?",
+        image: "images/pic59.jpg",
+        answer: "Lorenc",
+        details: "Film: Na samotě u lesa (1976). Pane Lorenc, uvezu to? Ne."
+    },
+    {
+        question: "Celé jméno herečky, která hrála roli Elišky Pomořanské?",
+        image: "images/pic60.jpg",
+        answer: "Jana Brejchová",
+        details: "Film: Noc na Karlštejně (1973)."
+    }
+    // Add more questions as needed
+];
+
+window.quizData = quizData;
